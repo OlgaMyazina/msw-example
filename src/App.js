c784975929cfe8e16c807fbb87da990c94822509
Olga <olga.myazina@outlook.com>
2022-11-18 13:36:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+if (process.env.NODE_ENV === 'development') {
+    const {worker} = require('./mocks/browser')
+    worker.start()
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const getResult = () => {
+        fetch('/test').then(resp => resp.json()).then(data => console.log(data))
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <button onClick={getResult}>Click me!</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
